60d634544f2fd14c4ad0ca71a9bba90b13ab8515
hwang <hhs1992@naver.com>
2020-08-12 23:37:20 +0900

    [dev] init commit

---
+++ network/Assets/Client.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    public GameObject chatContainer;
+    public GameObject msgPrefab;
+
+    private bool _socketReady;
+    private TcpClient _socket;
+    private NetworkStream _stream;
+    private StreamWriter _writter;
+    private StreamReader _reader;
+
+
+    private void Update()
+    {
+        if (_socketReady)
+        {
+            if (_stream.DataAvailable)
+            {
+                string data = _reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data);
+            }
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        Debug.Log($"----> server: {data}");
+        var go = Instantiate(msgPrefab, chatContainer.transform) as GameObject;
+        if (go != null && !string.IsNullOrEmpty(data))
+            go.GetComponentInChildren<Text>().text = data;
+    }
+
+    public void OnConnectedToServer()
+    {
+        if (_socketReady)
+            return;
+
+        string host = "127.0.0.1";
+        int port = 8000;
+
+        try
+        {
+            _socket = new TcpClient(host, port);
+            _stream = _socket.GetStream();
+            _writter = new StreamWriter(_stream);
+            _reader = new StreamReader(_stream);
+
+            _socketReady = true;
+            Debug.Log("----> client connect");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("----> error: " + e);
+        }
+    }
+
+    private void Send(string data)
+    {
+        if (!_socketReady)
+            return;
+
+        _writter.WriteLine(data);
+        _writter.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        string msg = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        Send(msg);
+    }
+
+    private void CloseSocket()
+    {
+        if (!_socketReady)
+            return;
+
+        _writter.Close();
+        _reader.Close();
+        _socket.Close();
+
+        _socketReady = false;
+    }
+
+    private void OnApplicationQuit() => CloseSocket();
+    private void OnDisable() => CloseSocket();
+        
+}
---
+++ network/Assets/Server.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    private List<ServerClient> _clients;
+    private List<ServerClient> _disconnectList;
+
+    public int port = 8000;
+    private TcpListener _server;
+    private bool _serverStarted;
+
+    private void Start()
+    {
+        _clients = new List<ServerClient>();
+        _disconnectList = new List<ServerClient>();
+
+        try
+        {
+            _server = new TcpListener(IPAddress.Any, port);
+            _server.Start();
+
+            StartListening();
+
+            _serverStarted = true;
+            Debug.Log("----> server start");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("----> Socket error: " + e);
+        }
+    }
+
+    private void Update()
+    {
+        if (!_serverStarted)
+            return;
+
+        foreach (var c in _clients)
+        {
+            if (!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                _disconnectList.Add(c);
+                continue;
+            }
+
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if (data != null)
+                    {
+                        OnIncomingData(c, data);
+                    }
+                }
+            }
+        }
+    }
+    private void OnIncomingData(ServerClient c, string data)
+    {
+        Debug.Log($"{c.clientName}: + {data}");
+        BroadCast(data, _clients);
+    }
+
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                else
+                    return true;
+            }
+            else
+                return false;
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"----> exception: {e}");
+            return false;
+        }
+    }
+
+    private void StartListening()
+    {
+        _server.BeginAcceptTcpClient(AcceptTcpClient, _server);
+    }
+
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+        _clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+        StartListening();
+
+        BroadCast($"{_clients[_clients.Count - 1].clientName} has connected"
+            , _clients);
+    }
+
+    private void BroadCast(string data, List<ServerClient> cl)
+    {
+        foreach (var c in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(c.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"----> write error: {e.Message} + to client: {c.clientName}");
+            }
+        }
+    }
+}
+
+public class ServerClient
+{
+    public TcpClient tcp;
+    public string clientName;
+
+    public ServerClient(TcpClient clientSocket)
+    {
+        if (string.IsNullOrEmpty(clientName))
+            clientName = "Guest";
+
+        tcp = clientSocket;
+    }
+
+}
